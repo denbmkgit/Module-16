4198b7aab494bfc65393eca52058c02ac8ce935e
denbmkgit <116366974+denbmkgit@users.noreply.github.com>
2024-12-01 00:14:27 +0300

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -9,13 +10,17 @@ async def Get_Admin_Page():
 
 
 @app.get("/user/{user_id}")
-async def Get_User_Number(user_id: str, ):
+async def Get_User_Number(user_id: int = Path(ge=1, le=100, description='Enter User ID', example='55')):
     return f'Вы вошли как пользователь № {user_id}'
 
 
-@app.get("/user/{user_name}/{age}")
-async def Get_User_Info(user_name: str, age=int):
-    return f'Информация о пользователе. Имя: {user_name}, Возраст: {age}'
+@app.get("/user/{username}/{age}")
+async def Get_User_Info(
+        username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')]
+        , age: int = Path(description='Enter age', example=24)):
+    # (user_name: str = Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser'),
+    # age: int = Path(description='Enter age', example=24)):
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
 
 
 @app.get("/")
