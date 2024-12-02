34f885a9731eb5aaf3ad5212f017999493ff8842
denbmkgit <116366974+denbmkgit@users.noreply.github.com>
2024-12-02 17:15:56 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -5,7 +5,7 @@ app = FastAPI()
 users = {'1': 'Имя: Example, возраст: 18'}
 
 
-@app.get("/")
+@app.get("/users")
 async def get_all_messages() -> dict:
     return users
 
@@ -13,13 +13,13 @@ async def get_all_messages() -> dict:
 @app.post("/user/{username}/{age}")
 async def add_user(username=str, age=int) -> str:
     current_index = str(int(max(users, key=int)) + 1)
-    users[current_index] = f'{username}, {age}'
+    users[current_index] = f'Имя: {username}, {age}'
     return f"User {current_index} is registered"
 
 
 @app.put('/user/{user_id}/{username}/{age}')
 async def update_user(user_id, username, age) -> str:
-    users[user_id] = f'{username}, {age}'
+    users[user_id] = f'{username}, возраст: {age}'
     return f"The user {user_id} is updated"
 
 
