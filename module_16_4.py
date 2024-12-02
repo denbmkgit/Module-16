974d7d3a0d897a617a2ed867ede96ba3d5bc7de5
denbmkgit <116366974+denbmkgit@users.noreply.github.com>
2024-12-02 11:38:50 +0300

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -8,7 +8,7 @@ users = []
 
 
 class User(BaseModel):
-    id: int =None
+    id: int = None  # проверить будет ли работать без None?
     username: str
     age: int
 
@@ -25,6 +25,7 @@ async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, de
         user_id = len(users) + 1
     else:
         user_id = 1
+    # user_id = users[-1]['id'] + 1 if users else 1  # Проверить так работает или нет и (понять)узнать почему работает- users[-1].id ?
     new_user = User(id=user_id, username=username, age=age)
     users.append(new_user)
     return new_user
@@ -32,13 +33,20 @@ async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, de
 
 @app.put('/user/{user_id}/{username}/{age}')
 async def update_user(user_id=int, username=str, age=int) -> str:
-    users[user_id] = f'{username}, {age}'
-    return f"The user {user_id} is updated"
+    try:
+        users[user_id] = f'{username}, {age}'
+        return f"The user {user_id} is updated"
+    except IndexError:
+        raise HTTPException(status_code=404, detail='User was not found')
 
 
 @app.delete('/user/{user_id}')
 async def delete_user(user_id):
-    users.pop(user_id)
+    try:
+        users.pop(user_id)
+        return user_id
+    except IndexError:
+        raise HTTPException(status_code=404, detail='User not found')
 
 #                                                  python -m uvicorn module_16_4:app
 
