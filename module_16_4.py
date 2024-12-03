5c734ac7188304dac2e42737acad8418284ffa5a
denbmkgit <116366974+denbmkgit@users.noreply.github.com>
2024-12-03 20:08:48 +0300

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -8,7 +8,7 @@ users = []
 
 
 class User(BaseModel):
-    id: int = None  # проверить будет ли работать без None?
+    id: int =None
     username: str
     age: int
 
@@ -25,28 +25,27 @@ async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, de
         user_id = len(users) + 1
     else:
         user_id = 1
-    # user_id = users[-1]['id'] + 1 if users else 1  # Проверить так работает или нет и (понять)узнать почему работает- users[-1].id ?
     new_user = User(id=user_id, username=username, age=age)
     users.append(new_user)
     return new_user
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id=int, username=str, age=int) -> str:
-    try:
-        users[user_id] = f'{username}, {age}'
-        return f"The user {user_id} is updated"
-    except IndexError:
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')],username: Annotated[str,
+                      Path(min_length=5, max_length=20, description='Enter username')], age: Annotated[int,
+                      Path(ge=18, le=120, description='Enter age')]) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    else:
         raise HTTPException(status_code=404, detail='User was not found')
 
 
 @app.delete('/user/{user_id}')
 async def delete_user(user_id):
-    try:
-        users.pop(user_id)
-        return user_id
-    except IndexError:
-        raise HTTPException(status_code=404, detail='User not found')
+    users.pop(user_id)
 
 #                                                  python -m uvicorn module_16_4:app
 
@@ -55,3 +54,4 @@ async def delete_user(user_id):
 # Post - формы - оформиить заказ в магазине (например). Еще считается что это отправка данных На сервер
 # Put  - по своей сути пытается что то обновить, заменить и так далее
 # Delete - удаляет
+
