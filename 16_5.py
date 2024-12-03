ca6158bf9dda2af08182962a9e54953b43051b82
denbmkgit <116366974+denbmkgit@users.noreply.github.com>
2024-12-03 22:03:53 +0300

    Update 16_5.py

--- 16_5.py
+++ 16_5.py
@@ -1,61 +1,63 @@
-from fastapi import FastAPI, status, Body, HTTPException
+from fastapi import FastAPI, Path, status, Body, HTTPException, Request, Form
+from fastapi.responses import HTMLResponse
+from typing import Annotated, List
 from pydantic import BaseModel
-from typing import List
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
-messages_db = []
+users = []
 
-
-class Message(BaseModel):
+class User(BaseModel):
     id: int = None
-    text: str
+    username: str
+    age: int
 
+@app.get('/')
+def get_all_users(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse('users.html',{'request': request, 'users': users})
 
-@app.get("/")
-def get_all_messages() -> List[Message]:
-    return messages_db
-
-
-@app.get(path="/message/{message_id}")
-def get_message(message_id: int) -> Message:
+@app.get('/user/{user_id}')
+def get_user(request: Request, user_id: int) -> HTMLResponse:
     try:
-        return messages_db[message_id]
+        return templates.TemplateResponse('users.html', {'request':request, 'user': users[user_id-1]})
     except IndexError:
         raise HTTPException(status_code=404, detail="Message not found")
 
+@app.post('/user/{username}/{age}') # запрос регистрация пользователя
+def create_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                    age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
 
-@app.post("/message")
-def create_message(message: Message) -> str:
-    message.id = len(messages_db)
-    messages_db.append(message)
-    return f"Message created!"
+    # new_id = (users[-1].id +1) if users else 1
+    if len(users) != 0:
+        new_id = len(users) + 1
+    else:
+        new_id = 1
+    new_user = User(id=new_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
 
+@app.put('/user/{user_id}/{username}/{age}') # запрос на изменение данных пользователя
+def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                      age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    else:
+        raise HTTPException(status_code=404, detail='Пользователя не существует')
 
-@app.put("/message/{message_id}")
-def update_message(message_id: int, message: str = Body()) -> str:
-    try:
-        edit_message = messages_db[message_id]
-        edit_message.text = message
-        return f"Message updated!"
-    except IndexError:
-        raise HTTPException(status_code=404, detail="Message not found")
-
-
-@app.delete("/message/{message_id}")
-def delete_message(message_id: int) -> str:
-    try:
-        messages_db.pop(message_id)
-        return f"Message ID={message_id} deleted!"
-    except IndexError:
-        raise HTTPException(status_code=404, detail="Message not found")
-
-
-@app.delete("/")
-def kill_message_all() -> str:
-    messages_db.clear()
-    return "All messages deleted!"
+@app.delete('/user/{user_id}') # запрос на удаление конкретного пользователя
+def delite_user(user_id: int) -> str:
+    for i, user in enumerate(users):
+        if user.id == user_id:
+            return users.pop(i)
+    else:
+        raise HTTPException(status_code=404, detail='Пользователя не существует')
 
 
 
-#                                                  python -m uvicorn 16.5:app
+#                                                  python -m uvicorn 16_5:app
